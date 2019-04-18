c39130f0b97ab48ec0c4c2b1de63eb49e8354425
kaiteli14 <kaiteli14@hotmail.com>
2019-04-18 16:03:01 -0500

    4:02pm

--- ParseTree/write_statement.py
+++ ParseTree/write_statement.py
@@ -8,16 +8,17 @@ from .common       import *
 
 #---------#---------#---------#---------#---------#--------#
 class write_statement() :
-  def __init__( self, lineNum, expression_opt) :
+  def __init__( self, lineNum, string_list) :
     self.m_NodeType = 'write_statement'
     self.m_LineNum = lineNum
-    self.m_expression_opt = expression_opt
-    self.m_expression_opt_size = len(expression_opt)
-    self.content = 'STATEMENT(WRITE)<' + self.m_expression_opt_size +'>'
+    self.m_string_list = string_list
+    self.m_string_list_size = len(string_list)
+
 #---------------------------------------
   def dump( self, indent = 0, fp = sys.stdout ) :
-    dumpHeaderLine(indent, self.m_LineNum,self.content, fp)
-    for m in self.m_expression_opt:
+    dumpHeaderLine(indent, self.m_LineNum,
+    f'STATEMENT(WRITE)<{self.m_string_list_size!r}>', fp)
+    for m in self.m_string_list:
       m.dump(indent+1, fp=fp)
 
 #---------#---------#---------#---------#---------#--------#
